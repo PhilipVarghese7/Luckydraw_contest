25dfac2e1deafd9e1304baf2320c8d01365fc1b9
PhilipVarghese7 <145372743+PhilipVarghese7@users.noreply.github.com>
2025-06-22 05:19:34 -0700

    Update urls.py(create admin)

--- draw/urls.py
+++ draw/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import create_admin_user
 from . import views
 
 urlpatterns = [
     path('', views.register, name='register'),  # 👈 form shown on home
     path('entries/', views.view_entries, name='view_entries'),
+    path('create-admin/', create_admin_user),
     path('pick-winner/', views.pick_winner, name='pick_winner'),
     path('winner/', views.show_winner, name='show_winner'),
 ]
